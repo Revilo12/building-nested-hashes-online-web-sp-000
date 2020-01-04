7aa9095f736c8e158981bb976f4881003141b8fd
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-04 18:03:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,5 @@
 def second_challenge
   epic_tragedy = {
-    epic_tragedy = {
    :montague => {
       :patriarch => {},
       :matriarch => {},
@@ -14,5 +13,4 @@ def second_challenge
       :heroine_friends => []
    }
 }
- 
 end
